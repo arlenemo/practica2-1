30818077dba2c1c308657c737016f80a2814f22c
Alvaro Colunga <alvaro.colunga@gmail.com>
2015-10-04 00:53:40 +0000

    SQLite

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,10 +10,20 @@ angular.module('starter.controllers', [])
     }
     
     $scope.guardar = function(persona){
+        
+        $cordovaSQLite.execute(db, 'INSERT INTO agenda (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado!";
+        }, function(error) {
+            $scope.statusMessage = "Error al guardar: " + error.message;
+        })
+        
+        /*
         console.log("Nombre: "+persona.nombre);
         console.log("Apellido: "+persona.apellido);
         console.log("Telefono: "+persona.telefono);
         console.log("Email: "+persona.email);
+        */
     }
     
 })
@@ -27,7 +37,23 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+  //$scope.chats = Chats.all();
+  $scope.chats = [];
+  
+  $cordovaSQLite.execute(db, 'SELECT * FROM agenda ORDER BY id DESC')
+       .then(
+          function(result) {
+             if (result.rows.length > 0) {
+                        $scope.chats = result.rows;
+                    }
+                },
+                function(error) {
+                    $scope.statusMessage = "Error on loading: " + error.message;
+                }
+        );
+  
+  
+  
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
